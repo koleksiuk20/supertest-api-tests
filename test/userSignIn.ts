26d96d3a934cc3c49dde0de892ee449159689e69
Kamil Oleksiuk <koleksiuk20@gmail.com>
2023-02-07 20:06:06 +0100

    Add test for signing in with too short username and/or password

--- test/userSignIn.ts
+++ test/userSignIn.ts
@@ -1,11 +1,11 @@
-import { apiServer } from '../utils/config'
+import { apiServer, usersSignInPath } from '../utils/config'
 import { expect } from 'chai'
 
 describe('POST /users/signin', () => {
-  it('should return 200 when valid username and password', async () => {
+  it('should return 200 when signing in with valid username and password', async () => {
     const admin = 'admin'
     const response = await apiServer
-      .post('/users/signin')
+      .post(usersSignInPath)
       .send({ username: admin, password: admin })
 
     expect(response.status).to.eq(200)
@@ -13,10 +13,23 @@ describe('POST /users/signin', () => {
     expect(response.body.firstName).to.eq('Jan')
   })
 
-  it('should return 422 when invalid username and/or password', async () => {
+  it('should return 400 when signing in with too short username and/or password', async () => {
     const response = await apiServer
-      .post('/users/signin')
-      .send({ username: 'invalid username', password: 'invalid password' })
+      .post(usersSignInPath)
+      .send({ username: 'Te', password: 'St' })
+
+    expect(response.status).to.eq(400)
+    expect(response.body).to.deep.eq({
+      "password": "Minimum password length: 4 characters",
+      "username": "Minimum username length: 4 characters"
+    })
+  })
+
+  it('should return 422 when signing in with invalid username and/or password', async () => {
+    const invalidData = 'invalid data'
+    const response = await apiServer
+      .post(usersSignInPath)
+      .send({ username: invalidData, password: invalidData })
 
     expect(response.status).to.eq(422)
     expect(response.body.message).to.eq('Invalid username/password supplied')
