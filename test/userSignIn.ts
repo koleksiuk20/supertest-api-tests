f2210a97f84e23408925464d140733dd45ffb40d
Kamil Oleksiuk <koleksiuk20@gmail.com>
2023-02-08 16:46:11 +0100

    Update tests for sign in feature

--- test/userSignIn.ts
+++ test/userSignIn.ts
@@ -1,35 +1,51 @@
-import { apiServer, usersSignInPath } from '../utils/config'
 import { expect } from 'chai'
+
+import { registerUser } from '../helpers/registerUser'
+
+import { apiServer, signInPath } from '../utils/config'
+import { getClientUser } from '../utils/user'
+
 
 describe('POST /users/signin', () => {
   it('should return 200 when signing in with valid username and password', async () => {
     const admin = 'admin'
     const response = await apiServer
-      .post(usersSignInPath)
+      .post(signInPath)
       .send({ username: admin, password: admin })
 
     expect(response.status).to.eq(200)
     expect(response.body.username).to.eq('admin')
-    expect(response.body.firstName).to.eq('Jan')
+    expect(response.body.firstName).to.eq('Slawomir')
   })
 
-  it('should return 400 when signing in with too short username and/or password', async () => {
+  it('should return 200 when signing in with freshly created account', async () => {
+    const newUser = getClientUser()
+    await registerUser(newUser)
+
     const response = await apiServer
-      .post(usersSignInPath)
-      .send({ username: 'Te', password: 'St' })
+      .post(signInPath)
+      .send({ username: newUser.username, password: newUser.password })
+
+      expect(response.status).to.eq(200)
+      expect(response.body.username).to.eq(newUser.username)
+      expect(response.body.firstName).to.eq(newUser.firstName)
+  })
+
+  it('should return 400 when signing in with too short username', async () => {
+    const response = await apiServer
+      .post(signInPath)
+      .send({ username: 'ka', password: 'admin'})
 
     expect(response.status).to.eq(400)
     expect(response.body).to.deep.eq({
-      "password": "Minimum password length: 4 characters",
       "username": "Minimum username length: 4 characters"
     })
   })
 
   it('should return 422 when signing in with invalid username and/or password', async () => {
-    const invalidData = 'invalid data'
     const response = await apiServer
-      .post(usersSignInPath)
-      .send({ username: invalidData, password: invalidData })
+      .post(signInPath)
+      .send({ username: 'invalid username', password: 'invalid password' })
 
     expect(response.status).to.eq(422)
     expect(response.body.message).to.eq('Invalid username/password supplied')
